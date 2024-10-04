f7a578f6580f48586aa9adb52b93c3e37fcdca4f
ali <alawe88g@gmail.com>
2024-10-04 19:28:19 +0300

    first proj

---
+++ parts/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="./index.php">
+    <link rel="stylesheet" href="./css/style.css">
+    
+    
+    
+    
+    <title>علي محمد الغامدي</title>
+
+</head>
+
+
+<body>
